e28705aff54cc55ceaaa890867a8cc98397d8679
Oliver Smith <osmith@sysmocom.de>
2020-02-21 10:06:14 +0100

    IMSIPseudo.java: show hello world on LU

--- sim-applet/src/org/osmocom/IMSIPseudo/IMSIPseudo.java
+++ sim-applet/src/org/osmocom/IMSIPseudo/IMSIPseudo.java
@@ -16,9 +16,8 @@ public class IMSIPseudo extends Applet implements ToolkitInterface, ToolkitConst
 	// which has a limited number of write cycles.
 	private byte helloMenuItem;
 
-	static byte[] welcomeMsg = new byte[] { 'W', 'e', 'l', 'c', 'o', 'm', 'e', ' ',
-                                            't', 'o', ' ', 'T', 'o', 'o', 'r', 'C',
-                                            'a', 'm', 'p', ' ', '2', '0', '1', '2' };
+	static byte[] welcomeMsg = new byte[] { 'H', 'e', 'l', 'l', 'o', ',', ' ',
+						'W', 'o', 'r', 'l', 'd', '!' };
 	static byte[] menuItemText = new byte[] { 'I', 'M', 'S', 'I', ' ', 'P', 's', 'e', 'u', 'd', 'o', 'n', 'y', 'm',
 						  'i', 'z', 'a', 't', 'i', 'o', 'n'};
 
@@ -30,12 +29,14 @@ public class IMSIPseudo extends Applet implements ToolkitInterface, ToolkitConst
 		// Define the applet Menu Entry
 		helloMenuItem = reg.initMenuEntry(menuItemText, (short)0, (short)menuItemText.length,
 				PRO_CMD_SELECT_ITEM, false, (byte)0, (short)0);
+		reg.setEvent(EVENT_EVENT_DOWNLOAD_LOCATION_STATUS);
 	}
 
 	// This method is called by the card when the applet is installed. You must
 	// instantiate your applet and register it here.
 	public static void install(byte[] bArray, short bOffset, byte bLength) {
 		IMSIPseudo applet = new IMSIPseudo();
+
 		applet.register();
 	}
 
@@ -57,6 +58,13 @@ public class IMSIPseudo extends Applet implements ToolkitInterface, ToolkitConst
 			if (selectedItemId == helloMenuItem) {
 				showHello();
 			}
+		}
+
+		if (event == EVENT_EVENT_DOWNLOAD_LOCATION_STATUS) {
+			/* TODO: count the location updates done with the same
+			 * pseudo IMSI, and warn the user if it becomes too
+			 * high */
+			showHello();
 		}
 	}
 
